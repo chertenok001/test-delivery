dce70666f295180f7c4ad315c77d8691418d9959
chertenok001 <https://github.com/chertenok001/simplefood.git>
2024-10-04 14:56:48 +0300

    added section popular

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,15 @@ $(function () {
   )
   wow.init();
 
+  window.onscroll = function showHeader() {
+    var header__top = document.querySelector(".header__top");
+    if (window.pageYOffset > 0) {
+      header__top.classList.add("header__fixed");
+    } else {
+      header__top.classList.remove("header__fixed");
+    }
+  };
+
  
 });
 
